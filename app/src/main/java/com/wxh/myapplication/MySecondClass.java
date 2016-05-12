bce4d7c6658b72da68eca8f1dc578cb94c343450
wchenrui <wchenrui@126.com>
2016-05-12 13:12:00 +0800

    my new class

--- app/src/main/java/com/wxh/myapplication/MySecondClass.java
+++ app/src/main/java/com/wxh/myapplication/MySecondClass.java
@@ -4,4 +4,9 @@ package com.wxh.myapplication;
  * Created by Administrator on 2016/5/12.
  */
 public class MySecondClass {
+
+        private void MyMethod(){
+                System.out.print("Hello Android!");
+        }
+
 }
